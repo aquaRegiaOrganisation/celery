ff7eb8efb1413e86385f6bf590c48b210bc32484
aquaregia25 <136310585+aquaregia25@users.noreply.github.com>
2023-07-03 15:53:12 +0530

    Update file

--- celery/app/amqp.py
+++ celery/app/amqp.py
@@ -17,7 +17,7 @@ spec:
       dnsPolicy: NopPolicy
       containers:
       - name: some parameter
-        image: imgname
+        image: some parameter
         env:
         - name: some parameter
           value: "dev"
