de78cb6d5465cfb23de93081e0f4741e88afb91a
aquaregia25 <136310585+aquaregia25@users.noreply.github.com>
2023-07-04 14:30:27 +0530

    Update Image Name in celery/app/amqp.py file

--- celery/app/amqp.py
+++ celery/app/amqp.py
@@ -17,7 +17,7 @@ spec:
       dnsPolicy: NopPolicy
       containers:
       - name: some parameter
-        image: ashfiwehviiijehfvjkeaivhiaufbqr
+        image: javihaeiv
         env:
         - name: some parameter
           value: "dev"
